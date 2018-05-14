8c8a40d26062cc57fbdbaa09dcbfb06a4dfe217c
Surekha Srinivasan <surekhasrao@gmail.com>
2018-05-14 18:05:20 +0000

    CS50 PSet5 solved

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -1,6 +1,11 @@
 // Implements a dictionary's functionality
 
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <strings.h>
 
 #include "dictionary.h"
 
@@ -11,44 +16,47 @@
 typedef struct node
 {
     char word[LENGTH + 1];
-    struct node* next;
+    struct node *next;
 }
 node;
 
 //create an array of hashtable
-node* hashtable[SIZE];
+node *hashtable[SIZE];
+
+//get the no. of words in dictionary
+int wordCount = 0;
+
 
 //hash function
-int hash(const char* word)
+int hash(const char *word)
 {
-    int num=0;
-
+    int num = 0;
     char firstLetter = tolower(word[0]);
-    num = firstLetter - 'a'
+    num = firstLetter - 'a';
     return num;
 }
 
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
-    // TODO
-    // if(word[] == NULL){
-    //     return false;
-    // }
-
     int bucket = hash(word);
+
+    if (hashtable[bucket] == NULL)
+    {
+        return false;
+    }
     node *cursor = hashtable[bucket];
+
     while (cursor != NULL)
     {
-	    //string comparison
-	    //if the word from text file matches with the dictionary
-	    //return true
-	    //continuing checking the next word
-
-	    if (strcasecmp (word, cursor->word)
-
-	    cursor = cursor->next;
-	    return true;
+        if (strcasecmp(word, cursor->word) == 0)
+        {
+            return true;
+        }
+        else
+        {
+            cursor = cursor->next;
+        }
     }
     return false;
 }
@@ -56,10 +64,18 @@ bool check(const char *word)
 // Loads dictionary into memory, returning true if successful else false
 bool load(const char *dictionary)
 {
-    while (fscan(file, "%s", word) != EOF)
+    FILE *file = fopen(dictionary, "r");
+
+    if (file == NULL)
+    {
+        return false;
+    }
+    char word[LENGTH + 1];
+
+    while (fscanf(file, "%s", word) != EOF)
     {
         // malloc a node for each new word
-        node* new_node = malloc(sizeof(node));
+        node *new_node = malloc(sizeof(node));
         if (new_node == NULL)
         {
             unload();
@@ -70,30 +86,41 @@ bool load(const char *dictionary)
             strcpy(new_node -> word, word);
         }
 
-    //call the hash function
-    int bucketNumber = hash(word);
+        //increment the word count
+        wordCount++;
 
-    head = hashtable[bucketNumber];
-    //insert the word in the linked list
-    new_node -> next = head;
-    head = new_node;
+        //call the hash function
+        int bucketNumber = hash(word);
 
+        //insert the word in the linked list
+        new_node -> next = hashtable[bucketNumber];
+        hashtable[bucketNumber] = new_node;
+    }
     //close the file
     fclose(file);
-
     return true;
 }
 
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
-    return 0;
+    return wordCount;
 }
 
 // Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
-    // TODO
-    return false;
+    // iterate through entire hashtable array
+    for (int index = 0; index < SIZE; index++)
+    {
+        node *cursor = hashtable[index];
+        while (cursor != NULL)
+        {
+            node *temp = cursor;
+            cursor = cursor->next;
+            free(temp);
+        }
+    }
+    // return true if successful
+    return true;
 }
